9748cb71f2cb3a1c70e3af0a97a44bc16a763fdc
gus2d <gustavodone@hotmail.com>
2022-05-19 10:42:25 -0300

    Projeto exercício do curso

---
+++ Aprendendo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Aprendendo.Entities.Exceptions;
+
+namespace Aprendendo.Entities
+    
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine());
+
+                Account account = new Account(number, holder, balance, limit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                account.Withdraw(amount);
+
+                Console.WriteLine("New balance: " + account.Balance.ToString("f2"));
+            }
+
+            catch (DomainException ex)
+            {
+                Console.WriteLine("Withdraw error: " + ex.Message);
+            }
+
+        }
+    }
+}
